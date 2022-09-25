24f86209cb3d0c99b1f28b86ed7880d973530872
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2022-09-25 17:01:20 +0300

    no tmp?

--- index.js
+++ index.js
@@ -76,10 +76,10 @@ app.get('/download/:id', (req, res) => {
     // });
 
     const stream = ytdl(`https://youtube.com/watch?v=${req.params.id}`, { quality: 'highestaudio' });
-    stream.pipe(createWriteStream(`tmp/${req.params.id}.mp3`))
+    stream.pipe(createWriteStream(`${req.params.id}.mp3`))
         .on('finish', () => {
             console.log('downloaded ' + req.params.id);
-            res.download(path.join(__dirname,`/tmp/${req.params.id}.mp3`));
+            res.download(path.join(__dirname,`/${req.params.id}.mp3`));
         });
 });
 
